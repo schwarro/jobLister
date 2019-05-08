755b1291526f1f6eb77e64473cb36dbcdb823548
schwarro <rob.ap.schwartz@gmail.com>
2019-05-08 13:57:50 -0400

    creatd basic db and set up html for main page

---
+++ templates/inc/footer.php
@@ -0,0 +1,8 @@
+        </div> <!-- /container -->
+      </main>
+      <footer class="container">
+        <p>&copy; 2019 JobLister</p>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ templates/inc/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>JobLister</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/flatly/bootstrap.min.css">
+    <link rel="stylesheet" herf="css/styles/css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="create.php">Create Listing</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
