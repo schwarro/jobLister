4c269d5d778260a85dae207860f2b9b7bf11a5eb
schwarro <rob.ap.schwartz@gmail.com>
2019-05-08 15:44:56 -0400

    created classes and connected db to html

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -5,7 +5,7 @@
   //Main jumbotron for a primary marketing message or call to action
   <div class="jumbotron">
     <div class="container">
-      <h1 class="display-3">JobLister</h1>
+      <h1 class="display-3"><?php echo SITE_TITLE; ?></h1>
       <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
       <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
     </div>
@@ -13,35 +13,17 @@
 
   <div class="container">
     <!-- Example row of columns -->
-    <div class="row">
-      <div class="col-md-10">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+    <?php foreach($jobs as $job): ?>
+      <div class="row">
+        <div class="col-md-10">
+          <h4><?php echo $job->job_title; ?></h4>
+          <p><?php echo $job->description; ?></p>
+        </div>
+        <div class="col-md-2">
+          <a class="btn btn-primary" href="#">View</a>
+        </div>
       </div>
-      <div class="col-md-2">
-        <a class="btn btn-primary" href="#">View</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-10">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-      </div>
-      <div class="col-md-2">
-        <a class="btn btn-primary" href="#">View</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-10">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-      </div>
-      <div class="col-md-2">
-        <a class="btn btn-primary" href="#">View</a>
-      </div>
-    </div>
+    <?php endforeach; ?>
 
     <hr>
 
